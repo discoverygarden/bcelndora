51fc65cd4e0c7967913694e134a218008cf3d350
Chris MacDonald <31731869+chrismacdonaldw@users.noreply.github.com>
2024-11-22 09:14:42 -0400

    Adjust so objects are exported to a single folder instead.

--- docs/resources/namespace_split.sh
+++ docs/resources/namespace_split.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-FEDORA_DATA="${FEDORA_DATA:-/usr/local/fedora/data}";
+FEDORA_DATA="${FEDORA_DATA:-/usr/local/fedora/data}"
+EXPORT_DIR="${FEDORA_DATA}/exports"
 
 if [[ -z "${NAMESPACES}" ]]; then
     echo "Error: NAMESPACES needs to be defined, and should be a comma separated list of namespaces."
@@ -8,22 +9,22 @@ if [[ -z "${NAMESPACES}" ]]; then
 fi
 
 if [[ ! -d "${FEDORA_DATA}/objectStore" ]]; then
-    echo "Error: The objectStore directory does not exist at $FEDORA_DATA/objectStore".
-    exit 1;
+    echo "Error: The objectStore directory does not exist at $FEDORA_DATA/objectStore"
+    exit 1
 fi
 
 IFS=',' read -r -a namespaces <<< "$NAMESPACES"
 
+mkdir -p "$EXPORT_DIR"
+
+cd "${FEDORA_DATA}/objectStore" || exit 1
+
 for NAMESPACE in "${namespaces[@]}"; do
-    mkdir -p "$FEDORA_DATA/$NAMESPACE"
-
-    cd "${FEDORA_DATA}/objectStore" || exit 1
-
-    find . -name "info%3Afedora%2F${NAMESPACE}%3A*" > "$FEDORA_DATA/$NAMESPACE-filelist"
-
-    cd $FEDORA_DATA
-
-    while read -r i; do
-        rsync -R "objectStore/$i" "$NAMESPACE/"
-    done < "$FEDORA_DATA/$NAMESPACE-filelist"
+    find . -name "info%3Afedora%2F${NAMESPACE}%3A*" >> "$EXPORT_DIR/filelist"
 done
+
+cd $FEDORA_DATA
+
+while read -r i; do
+    rsync -R "objectStore/$i" "$EXPORT_DIR/"
+done < "$EXPORT_DIR/filelist"
