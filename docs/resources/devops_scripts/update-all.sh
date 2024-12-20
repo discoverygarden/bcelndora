20caa48732669ae6386698f3d76d34122b8d4c2d
Alexander Cairns <alexandercairns@discoverygarden.ca>
2024-12-20 16:16:46 -0400

    store updated script

--- docs/resources/devops_scripts/update-all.sh
+++ docs/resources/devops_scripts/update-all.sh
@@ -5,6 +5,17 @@ SCRIPTS_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
 
 namespace=${1?"A namespace is required"}
 
+echo Checking if extra manifests need updates.
+if ! kubectl diff -n $namespace -f $namespace/extras.yaml; then
+  echo Updates to the extra kubernetes manifests are avilable or kubectl diff failed to run.
+  read -p "Apply Updates (y/n)?" choice
+  case "$choice" in
+    y|Y ) kubectl apply -n $namespace -f $namespace/extras.yaml;;
+    n|N ) ;;
+    * ) echo "invalid"; exit 1;;
+  esac
+fi
+
 charts_file=$namespace/charts.yaml
 if [ ! -f $charts_file ]; then
   echo Charts file $charts_file does not exist
