5279a70a0d279687b2226e46f243b9298ff83cb8
Adam Bowman <adambowman@discoverygarden.ca>
2025-07-30 11:55:54 -0300

    Improve log retrieval in export-config script by adding follow flag to kubectl logs command

--- docs/resources/devops_scripts/export-config.sh
+++ docs/resources/devops_scripts/export-config.sh
@@ -17,7 +17,7 @@ job_status=$(kubectl get job $job_name -n $ns -o jsonpath='{.status.succeeded}')
 if [[ "$job_status" == "1" ]]; then
   pod_name=$(kubectl get pods -n $ns --selector=job-name=$job_name -o jsonpath='{.items[0].metadata.name}')
   echo "Job succeeded. Tailing logs:"
-  kubectl logs -n $ns $pod_name
+  kubectl logs -f -n "$ns" "$pod_name"
   exit 0
 else
   echo "Job failed or did not complete successfully."
