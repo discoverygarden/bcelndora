e68a1a9115f211eabd31d00aed0dcf32638cb230
Adam Bowman <adambowman@discoverygarden.ca>
2025-07-30 11:59:19 -0300

    Add job existence check and cleanup in export-config script

--- docs/resources/devops_scripts/export-config.sh
+++ docs/resources/devops_scripts/export-config.sh
@@ -6,6 +6,14 @@ ns=${1?"A namespace is required"}
 cronjob_name="bceln-drupal--config-export-cron"
 job_name="${ns}-config-export-$(date +%s)"
 
+# Check if a job with the same name already exists in the namespace
+if kubectl get job "$job_name" -n "$ns" &>/dev/null; then
+  echo "A job named '$job_name' already exists in namespace '$ns'. Remove existing job before running this script."
+  echo "You can remove it with: kubectl delete job $job_name -n $ns"
+  echo "Exiting..."
+  exit 1
+fi
+
 echo "Creating a manual Job from CronJob '$cronjob_name' in namespace '$ns'..."
 kubectl create job --from=cronjob/$cronjob_name $job_name -n $ns
 
@@ -18,6 +26,7 @@ if [[ "$job_status" == "1" ]]; then
   pod_name=$(kubectl get pods -n $ns --selector=job-name=$job_name -o jsonpath='{.items[0].metadata.name}')
   echo "Job succeeded. Tailing logs:"
   kubectl logs -f -n "$ns" "$pod_name"
+  kubectl delete job "$job_name" -n "$ns" --wait=false
   exit 0
 else
   echo "Job failed or did not complete successfully."
